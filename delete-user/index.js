fe237167de096277884439f53ffbdfb1e3e55404
Ronan Hughes <streamin@Ronans-MacBook-Pro.local>
2020-02-01 19:40:55 +0000

    delete user

---
+++ delete-user/index.js
@@ -0,0 +1,18 @@
+const request = require('request');
+
+exports.deleteUser = (data, context) => {
+  
+  if (context.params.user === undefined) {
+    res.status(400).send('No user defined!');
+  } else {
+    
+    console.log(context.params.user);
+    var url = 'https://user-matching-service-gqdj5a23xq-ew.a.run.app/match/deleteUser?user=';
+    var fullURL = url.concat(context.params.user);
+    request.get(fullURL, function (error, response, body) {
+      console.log('error:', error); // Print the error if one occurred 
+      console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received 
+    });
+    
+  }
+};
